edcec654d07d64aa39d659df19de29b9fe07ba24
João Lutz <jaflutz@gmail.com>
2014-01-03 18:26:48 -0200

    Test fix

--- spec/lib/directory_manager_spec.rb
+++ spec/lib/directory_manager_spec.rb
@@ -6,9 +6,10 @@ describe "DirectoryManager" do
   let(:directory_content) { ["teste.mkv", "teste2.srt", "teste3.mkv"] }
 
   it "reads the correct files of a specified filetype" do
-    DirectoryManager.stub(:path).and_return("anything")
+    directory_manager = DirectoryManager.new("./")
+    directory_manager.stub(:path).and_return("anything")
     Dir.stub(:entries).with(anything()).and_return(directory_content)
-    expect(DirectoryManager.find_files(/.mkv/)).to eq(["teste.mkv", "teste3.mkv"])
+    expect(directory_manager.find_files(/.mkv/)).to eq(["teste.mkv", "teste3.mkv"])
   end
 
 end
